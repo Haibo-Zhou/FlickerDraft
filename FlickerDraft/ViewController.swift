953c186448164a66e6171dc99bc8c65afbaf465b
haibo.zhou <chuckzhb@hotmail.com>
2020-01-26 15:57:45 +0800

    Initial Commit

---
+++ FlickerDraft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerDraft
+//
+//  Created by Chuck.Zhou on 1/26/20.
+//  Copyright © 2020 Haibo Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
